6b1e8396ee858e15a62608803311a442dc4efdd5
Damian <dzoltow@gmail.com>
2022-05-03 23:59:06 +0200

    added missing files

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "4o6DauSoLpiLaStihjWiL8",
+});
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+});
---
+++ components/Player.js
@@ -0,0 +1,150 @@
+import {
+  VolumeUpIcon as VolumeDownIcon,
+  HeartIcon,
+} from "@heroicons/react/outline";
+import {
+  RewindIcon,
+  VolumeUpIcon,
+  SwitchHorizontalIcon,
+  ReplyIcon,
+  PlayIcon,
+  PauseIcon,
+  FastForwardIcon,
+} from "@heroicons/react/solid";
+import { debounce } from "lodash";
+import { useSession } from "next-auth/react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import useSongInfo from "../hooks/useSongInfo";
+import useSpotify from "../hooks/useSpotify";
+
+function Player() {
+  const spotifyApi = useSpotify();
+  const { data: session } = useSession();
+  const songInfo = useSongInfo();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(50);
+
+  const fetchCurrentSong = (requested) => {
+    console.log("called");
+    if (!songInfo || requested) {
+      spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+        console.log("Now playing", data.body?.item);
+        setCurrentTrackId(data.body?.item?.id);
+      });
+
+      spotifyApi.getMyCurrentPlaybackState().then((data) => {
+        setIsPlaying(data.body?.is_playing);
+      });
+    }
+  };
+
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body?.is_playing) {
+        spotifyApi.pause();
+        setIsPlaying(false);
+      } else {
+        spotifyApi.play();
+        setIsPlaying(true);
+      }
+    });
+  };
+
+  const handleSkipTrack = async (wasSkipNext) => {
+    if (wasSkipNext) {
+      spotifyApi.skipToNext();
+    } else {
+      spotifyApi.skipToPrevious();
+    }
+    setTimeout(() => fetchCurrentSong(true), 1000);
+  };
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+      fetchCurrentSong();
+      setVolume(50);
+    }
+  }, [currentTrackId, spotifyApi, session]);
+
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume);
+    }
+  }, [volume]);
+
+  const debouncedAdjustVolume = useCallback(
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((err) => {});
+    }, 500),
+    []
+  );
+
+  return (
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
+      {/* Left */}
+      <div className="flex items-center space-x-4 ">
+        <img
+          className="hidden md:inline h-10 w-10"
+          src={songInfo?.album?.images?.[0]?.url}
+          alt=""
+        />
+        <div>
+          <h3>{songInfo?.name}</h3>
+          <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
+      </div>
+
+      {/* Center */}
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button" />
+        <RewindIcon
+          onClick={() => {
+            handleSkipTrack(false);
+          }}
+          className="button"
+        />
+        {isPlaying ? (
+          <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        ) : (
+          <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        )}
+
+        <FastForwardIcon
+          onClick={() => {
+            handleSkipTrack(true);
+          }} // might be brokenn
+          className="button"
+        />
+        <ReplyIcon className="button" onClick={() => fetchCurrentSong(true)} />
+      </div>
+
+      {/* Right */}
+
+      <div className="flex items-center space-x-3 md:space-x-4 pr-5 justify-end">
+        <VolumeDownIcon
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+          className="button"
+        />
+        <input
+          type="range"
+          value={volume}
+          min={0}
+          max={100}
+          className="w-14 md:w-28"
+          onChange={(e) => setVolume(Number(e.target.value))}
+        />
+
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Player;
